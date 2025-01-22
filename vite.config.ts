fd519913fc427fe57215b1e46731f97791574281
carlosWirama <carloswirama@gmail.com>
2025-01-23 06:11:51 +0800

    feat: vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,22 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
+	base: process.env.BASE_PATH || '/',
+  plugins: [
 		preact({
 			prerender: {
 				enabled: true,
 				renderTarget: '#app',
-				additionalPrerenderRoutes: ['/404'],
+				additionalPrerenderRoutes: ['/404'], // Add more static pages as needed
 				// previewMiddlewareEnabled: true,
 				previewMiddlewareFallback: '/404',
 			},
 		}),
 	],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    open: false, // Automatically open in your browser during dev
+  },
 });
